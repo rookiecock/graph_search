2cf79a3c5854fc935274eb292313449f986fb3ba
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-10-15 07:30:42 +0000

    overall_stats: add overall stats for all available cores

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,4 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascrip
+// or vendor/assets/javascripts of plugins, if any, can be referenced here usi
