9d7eac4d89180ecc6e90a2b1bf29a5fa7b5ddf7c
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-10-21 05:30:33 +0000

    5.4.1

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -2,4 +2,5 @@
 function format(stock){
     code = split(stock.text, ' ', 1)[0];
     name = split(stock.text, ' ', 1)[1];
-    name = name.subs
+    name = name.substring(1, name.length - 1);
+    if(stock.id == '') 
