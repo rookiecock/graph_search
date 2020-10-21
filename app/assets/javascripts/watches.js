5da536be92d6786015e11507658c04142a326f6e
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-10-21 13:14:37 +0000

    Added Travis CI build status

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -3,4 +3,5 @@ function format(stock){
     code = split(stock.text, ' ', 1)[0];
     name = split(stock.text, ' ', 1)[1];
     name = name.substring(1, name.length - 1);
-    if(stock.id == '') 
+    if(stock.id == '') return '<p>' + stock.text + '</p>';
+    return '<p class="s
