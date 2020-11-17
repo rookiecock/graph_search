67296f4940a41fc0d7802df5207af4606412ee2f
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-17 17:49:11 +0000

    Updated phpunit config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :e
+  protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_paramet
