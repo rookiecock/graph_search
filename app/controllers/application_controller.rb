66b3f1cb7da82141f982b44a996017637b9c9e62
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-17 19:55:57 +0000

    Improve the docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_paramet
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_p
