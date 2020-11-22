bfed28d238736172badecf0e73f336709305df52
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-22 06:15:20 +0000

    maj Dockerfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) {
-     |u| u.permit(:email, :password, :password_confirmation, :curre
+     |u| u.permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name) }
+   
