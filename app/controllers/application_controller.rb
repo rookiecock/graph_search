0961596b05ef2d2fe272e21c2c540a5a9a44c2c0
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-22 11:11:50 +0000

    #42[ci skip]prepare for next development iteration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) {
      |u| u.permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name) }
-   
+    devise_parameter_sanitizer.for(:sign_up) {
+     |
