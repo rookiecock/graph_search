17c550d6c1786c56d52e228ca35a1c80cd949df3
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-24 23:09:58 +0000

    Fix order of arguments in assertEqual

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,3 +1,4 @@
 class WatchesController < ApplicationController
   before_action :set_watch, only: [:show, :edit, :update, :destroy]
-  before_action :get_stocks, only: [:ne
+  before_action :get_stocks, only: [:new, :edit, :create, :update]
+  before_filter :authenti
