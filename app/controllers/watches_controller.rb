643e67b5d97923e1024d03aff52ef4812bdfb974
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-11-25 17:47:05 +0000

    Stuff

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,4 +1,8 @@
 class WatchesController < ApplicationController
   before_action :set_watch, only: [:show, :edit, :update, :destroy]
   before_action :get_stocks, only: [:new, :edit, :create, :update]
-  before_filter :authenti
+  before_filter :authenticate_user!
+
+  # GET /watches
+  def index
+    @wa
