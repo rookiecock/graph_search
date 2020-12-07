e0ca8c7bd7d4ada96554b68e25ec3e32de04b68f
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-12-07 11:28:37 +0000

    Remove old migrations

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -59,4 +59,8 @@ class WatchesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_watch
-      @watch = Watc
+      @watch = Watch.find(params[:id])
+    end
+
+    def get_stocks
+  
