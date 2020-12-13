952171ced8b5d577e1bc6d903843a8f2987e0e54
rookiecock <106289526+rookiecock@users.noreply.github.com>
2020-12-13 18:15:09 +0000

    Added blog

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -3,4 +3,7 @@ class AlertMailer < ActionMailer::Base
 
   def stock_alert(watch, daily_diff)
     @daily_diff = daily_diff
-    @quote = daily_diff.
+    @quote = daily_diff.quote
+    @watch = watch
+
+  	@direction_up = daily_diff.d
