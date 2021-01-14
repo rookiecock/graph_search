a5fb3a6a9b49d670c690ada54e1400c7a84fa675
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-01-14 08:37:28 +0000

    timekeeping: Add missing update call in timekeeping_resume()

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def fu
+  def full_name
+  	"#{first_name} #{last_name}"
+  end
