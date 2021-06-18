33c445f4a3f93d26e3a6d0b5c0153dbff234cdb2
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-06-18 22:24:48 +0000

    make release

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,7 @@ module Stockwatcher
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding 
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    #Time.zone = "Auckland"
+
+    # U
