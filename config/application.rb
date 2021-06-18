a591be23b3ddbec19af0d7878b4a586844b1194e
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-06-18 09:34:23 +0000

    Bump pytest from 6.2.4 to 6.2.5

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,4 @@ module Stockwatcher
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake 
+    # Run "rake -D time" for a list of tasks for finding 
