46e60334b4adafa99d966ee23d82aad47393a381
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-07-05 05:29:31 +0000

    Update RingQt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
 Stockwatcher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the developm
+  # In the development environment your application's code is 
