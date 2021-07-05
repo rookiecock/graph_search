3077d57690045c8ff6f7524b198c3dfc7bd85210
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-07-05 20:58:38 +0000

    add main file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,4 +2,4 @@ Stockwatcher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request. This 
+  # every request. This slows down response time but is perfect for dev
