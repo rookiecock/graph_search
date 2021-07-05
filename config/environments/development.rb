aa74599c5c7f1ea18003f63d888d4456de95207e
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-07-05 11:59:50 +0000

    minor touchups

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Stockwatcher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is 
+  # In the development environment your application's code is reloaded on
+  # every request. This 
