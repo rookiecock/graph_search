ae88bbe758f267afd0482356b9845bc4bb09942b
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-07-23 10:50:33 +0000

    Ariel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,5 @@ Stockwatcher::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  # Print d
+  # Print deprecation notices to the Rails logger.
+  config.active_
