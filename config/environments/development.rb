14fa3ba530b1d42f10abfd98988fc4e61daea171
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-05 22:39:55 +0000

    Create INSTALL.md

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,4 +31,5 @@ Stockwatcher::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'st
+    domain:               'stockwatcher.co.nz',
+    user_name:            'support@st
