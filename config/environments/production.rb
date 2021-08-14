48ba655a5d4c3d5c437da1a3754438ea10c1cf4d
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-14 17:42:55 +0000

    ping travis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,3 +61,31 @@ Stockwatcher::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found).
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+
+  # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'https://stockwatcher.co.nz' }
+  config.action_mailer.smtp_settings = {
+    address:              'email-smtp.us-west-2.amazonaws.com',
+    port:                 587,
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+end
