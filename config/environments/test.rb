d35173f9824eb0d77fee0283d2c911370a47381b
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-22 17:31:30 +0000

    cleaned up some code

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,2 +1,4 @@
 Stockwatcher::Application.configure do
-  # Settings specified here will take precedence over those in config/app
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # The test environment is used exclusively to ru
