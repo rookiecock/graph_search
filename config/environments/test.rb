f7a8778873adf91344b99b54501914a0c44b9777
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-25 18:00:56 +0000

    drm/edid/quirks: ViewSonic VA2026w

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,3 +3,4 @@ Stockwatcher::Application.configure do
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
+  # your test database is "scratch space" for the tes
