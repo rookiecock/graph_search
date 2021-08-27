4ac88fe10480454292824abcf54e7be97042df0e
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-27 15:48:26 +0000

    added return

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,4 +8,4 @@ Stockwatcher::Application.configure do
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
-  # just for th
+  # just for the purpose of running a single test. If you are us
