b198dde7189c0230eccf7188d047a8bd36a5c8e3
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-08-30 18:59:20 +0000

    f2fs: fix wrong statistics of inline data

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,4 +14,6 @@ Stockwatcher::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
-  config.static_cache_control = 
+  config.static_cache_control = "public, max-age=3600"
+
+  # Show full error reports an
