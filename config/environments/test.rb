48ea2384962f27d5f335c06857803e5170ef8428
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-09-01 10:46:10 +0000

    ALSA: usb-audio: fix Roland/Cakewalk UM-3G support

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,4 +18,6 @@ Stockwatcher::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering
