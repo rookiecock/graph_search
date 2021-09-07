0a8e37241c7a8455246a787cf2f5d8944761611f
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-09-07 19:44:10 +0000

    Remove some debugging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,4 +24,6 @@ Stockwatcher::Application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  c
+  config.action_controller.allow_forgery_protection = false
+
+  #
