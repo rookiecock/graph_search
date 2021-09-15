8d2baf199f03b1759824dbf8c40ba47306a4bd63
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-09-15 14:17:21 +0000

    :3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,6 @@ Stockwatcher::Application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_
+  config.action_mailer.delivery_method = :test
+
+  # Print deprecati
