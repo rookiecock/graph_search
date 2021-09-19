76f82e7bb37a6f44bd62004328e2797c824122ec
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-09-19 09:21:55 +0000

    change example

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,5 @@ Stockwatcher::Application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.de
+  config.active_support.deprecation = :stderr
+end
