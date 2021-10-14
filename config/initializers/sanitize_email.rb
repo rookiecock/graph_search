74d14d78043cd0dcb3ce15126c73655b30051472
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-10-14 17:36:00 +0000

    set up project

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -2,4 +2,5 @@ if Rails.env.development?
   SanitizeEmail::Config.configure do |config|
     config[:sanitized_to] =         'george@dewar.co.nz'
 
-    # run/call whatever logic should turn sanitize_email on and off in this Pr
+    # run/call whatever logic should turn sanitize_email on and off in this Proc:
+    config[:activation_proc] =      Proc.new { 
