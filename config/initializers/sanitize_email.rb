f36a08786ac61501a6f82112312d6f3b28597a87
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-10-14 08:52:16 +0000

    Table fix

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -1,3 +1,5 @@
 if Rails.env.development?
   SanitizeEmail::Config.configure do |config|
-    config[:sanitized_to] =     
+    config[:sanitized_to] =         'george@dewar.co.nz'
+
+    # run/call whatever
