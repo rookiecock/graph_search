2c1458f0f21dcced4d2c064c7d49e64c695811ce
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-10-14 09:16:04 +0000

    Fix package names

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -2,4 +2,4 @@ if Rails.env.development?
   SanitizeEmail::Config.configure do |config|
     config[:sanitized_to] =         'george@dewar.co.nz'
 
-    # run/call whatever
+    # run/call whatever logic should turn sanitize_email on and off in this Pr
