5441c492ab855f149ce8a27d72877b4379491340
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-10-16 07:10:51 +0000

    documentation formatting

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -3,4 +3,5 @@ if Rails.env.development?
     config[:sanitized_to] =         'george@dewar.co.nz'
 
     # run/call whatever logic should turn sanitize_email on and off in this Proc:
-    config[:activation_proc] =      Proc.new { 
+    config[:activation_proc] =      Proc.new { %w(development test).include?(Rails.env) }
+    config
