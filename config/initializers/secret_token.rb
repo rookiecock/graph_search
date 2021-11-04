86a876281af96d33893015ae68faaeee78608474
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-11-04 18:16:41 +0000

    User registration

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Stockwatcher::Application.config.secret_key_base = '2ddeb0b35536a3669cd631adcb0165ee3e0361b7e8e8c16b43bee0022f79b3
+Stockwatcher::Application.config.secret_key_base = '2ddeb0b35536a3669cd631adcb0165ee3e0361b7e8e8c16b43bee0022f79b3bc7829d9c6800343f015d0c410e1ed679e4900dfbce7c62a28bb3385baec5fa8e
