f1a499496feb2d402cb11ec197788756dd02b515
rookiecock <106289526+rookiecock@users.noreply.github.com>
2021-11-05 22:50:27 +0000

    Ajustes.

---
+++ config/initializers/task_scheduler.rb
@@ -0,0 +1,10 @@
+
+scheduler = Rufus::Scheduler.new
+
+scheduler.every("10m") do
+   	puts "--- #{DateTime.now} Updating prices ---\n"
+   	Quote.update_prices
+   	puts "--- Checking watches ---\n"
+   	Quote.check_watches
+   	puts "--- Done ---\n\n"
+end 
