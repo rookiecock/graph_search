9577be9c7346b83d49873b45df5353e335041d15
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-04-19 14:11:11 +0000

    Minor readme tweak

--- db/migrate/20140124072509_create_users.rb
+++ db/migrate/20140124072509_create_users.rb
@@ -3,4 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email,              :null => false, :default => ""
       t.string :first_name
-      t
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
