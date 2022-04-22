438a16d35199b54df2e2c5d5275ba481d913e4c0
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-04-22 09:24:09 +0000

    updated manifest

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -2,4 +2,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
-      #t.string :email,              :null => false, :default => "" 
+      #t.string :email,              :null => false, :default => "" ## email already existed
+      t.string :encrypted_
