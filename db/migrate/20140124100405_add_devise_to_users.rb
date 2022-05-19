31cee6d803893206b8a5dbd5a180a241dde2d745
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-05-19 05:26:34 +0000

    Add travis ci badge

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -14,4 +14,6 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
       ## Trackable
       t.integer  :sign_in_count, :default => 0, :null => false
-      t.datetime :current_sign_in_a
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string 
