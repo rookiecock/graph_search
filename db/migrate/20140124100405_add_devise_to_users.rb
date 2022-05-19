1d30aa4aba93e76c6d73c43b8a7ec39908257677
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-05-19 06:38:20 +0000

    edit

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -16,4 +16,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
       t.integer  :sign_in_count, :default => 0, :null => false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.string 
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+  
