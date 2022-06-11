ca226a3e5182582004a1374ff405a68bbed1cab7
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-06-11 14:18:04 +0000

    Add download links

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -27,4 +27,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is 
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+    
