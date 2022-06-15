d47304dcfa90112f69126d8335500b9516068036
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-06-15 12:03:45 +0000

    Add missing dependency

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -35,4 +35,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # t.timestamps
     end
 
-    add_index :users, :email,               
+    add_index :users, :email,                :unique => true
+    add_index :users,
