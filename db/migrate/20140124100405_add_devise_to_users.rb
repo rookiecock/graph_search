8fdc7cc6235908e64ee80fa68484f5c79254da2f
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-06-15 14:05:13 +0000

    Added testcase

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -36,4 +36,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users,
+    add_index :users, :reset_password_token, :unique => true
+ 
