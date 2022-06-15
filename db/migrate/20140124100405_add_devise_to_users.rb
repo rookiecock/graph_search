c1ff418822186d48a49cd8331be5a7fb9d737fb2
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-06-15 11:26:11 +0000

    _automated cleaning tests version comment spaces

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -33,4 +33,6 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps
-    en
+    end
+
+    add_index :users, :email,               
