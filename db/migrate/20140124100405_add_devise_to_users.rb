812f0af9fa7c382daad2c303f49b25c0e3cadd19
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-07-16 21:02:23 +0000

    SYSTEM: Plugin configuration files updated

--- db/migrate/20140124100405_add_devise_to_users.rb
+++ db/migrate/20140124100405_add_devise_to_users.rb
@@ -43,4 +43,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would l
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::Ir
