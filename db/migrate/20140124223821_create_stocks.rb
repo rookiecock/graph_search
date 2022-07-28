afb62471fb5a12339b2070e9623226a414c1978c
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-07-28 07:22:53 +0000

    Update the Language Model API, and implement it.

---
+++ db/migrate/20140124223821_create_stocks.rb
@@ -0,0 +1,12 @@
+
+class CreateStocks < ActiveRecord::Migration
+  def change
+    create_table :stocks do |t|
+      t.string :code, :null => false
+      t.string :name, :null => false
+
+      t.timestamps
+    end
+    add_index :stocks, :code, unique: true
+  end
+end
