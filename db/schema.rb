296221cdfe299209a4fd4a8610eecab74cdc6b76
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-10-28 12:13:27 +0000

    PDFBOX-2852: fix javadoc

--- db/schema.rb
+++ db/schema.rb
@@ -44,4 +44,6 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.datetime "updated_at"
   end
 
-  add_index "stocks", ["code"], name: "index_stocks_on_code", unique: 
+  add_index "stocks", ["code"], name: "index_stocks_on_code", unique: true, using: :btree
+
+  create_table "users", force: :cas
