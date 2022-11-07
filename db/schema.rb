4762d87da0fa181fdea57ce10cf1c5e0df798645
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-11-07 10:54:24 +0000

    :globe_with_meridians: l10n updated

--- db/schema.rb
+++ db/schema.rb
@@ -56,4 +56,5 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_coun
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.date
