8e56316f4d1f1201578b07aa4e546b6c0093e5ad
rookiecock <106289526+rookiecock@users.noreply.github.com>
2022-11-09 07:41:58 +0000

    Version 0

--- db/schema.rb
+++ db/schema.rb
@@ -63,4 +63,5 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", uni
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_
