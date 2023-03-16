ceb916e63a50e2aaaa942e40219398e05c03f131
rookiecock <106289526+rookiecock@users.noreply.github.com>
2023-03-16 16:31:11 +0000

    removed old code

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -3,4 +3,5 @@ require 'test_helper'
 class StockTest < ActiveSupport::TestCase
 
   test "should get latest quote" do
-    stock = stocks(:o
+    stock = stocks(:one)
+    assert stock.latest_quote.created_at.to_date =
